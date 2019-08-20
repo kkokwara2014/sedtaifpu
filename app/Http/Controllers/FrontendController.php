fa4bee0a834b7d1f32265aef03df325df53f2461
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-20 12:30:06 +0100

    --- Auto-Git Commit ---

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FrontendController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.index');
+    }
+}
