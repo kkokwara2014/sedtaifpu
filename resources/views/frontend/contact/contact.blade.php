d8c29b28592f89337c961d406387398735b2c6c7
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-20 17:02:08 +0100

    --- Auto-Git Commit ---

--- resources/views/frontend/contact/contact.blade.php
+++ resources/views/frontend/contact/contact.blade.php
@@ -9,55 +9,44 @@
             </div>
         </div>
 
-        <form action="#" id="registration-form">
-            <div class="row">
-                <div class="col-md-12" id="registration-msg" style="display:none;">
-                    <div class="alert"></div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Name" name="name" required>
+        <div class="row">
+            <div class="col-md-7 col-lg-7">
+                <form action="#" id="registration-form">
+                    <div class="row">
+                        <div class="col-md-12" id="registration-msg" style="display:none;">
+                            <div class="alert"></div>
+                        </div>
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your Name" name="name" required>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="Your Email Address" id="email"
+                                    name="email" required>
+                            </div>
+
+                            <div class="form-group">
+                                <textarea class="form-control" name="yourmessage" rows="5"
+                                    placeholder="Your Message"></textarea>
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Your Email Address" id="email"
-                            name="email" required>
+                    <div class="text-left">
+                        <button type="submit" class="btn btn-black" id="registration-submit-btn">Send</button>
                     </div>
-
-                    <div class="form-group">
-                        <textarea name="yourmessage" cols="30" rows="10" placeholder="Your Message"></textarea>
-                    </div>
-                </div>
-
-                <div class="col-sm-6">
-
-
-
-
-                    <div class="form-group">
-                        <select class="form-control" name="city" id="city" required>
-                            <option readonly>City</option>
-                            <option>City Name 1</option>
-                            <option>City Name 2</option>
-                            <option>City Name 3</option>
-                            <option>City Name 4</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <select class="form-control" name="program" id="program" required>
-                            <option readonly>Select Your Program</option>
-                            <option>Program Name 1</option>
-                            <option>Program Name 2</option>
-                            <option>Program Name 3</option>
-                        </select>
-                    </div>
-                </div>
+                </form>
             </div>
-            <div class="text-center mt20">
-                <button type="submit" class="btn btn-black" id="registration-submit-btn">Submit</button>
+
+            <div class="col-md-5 col-lg-5">
+               <p>You can reach us faster by getting in touch with</p> 
+                <p><span class="ion-ios-email" style="font-size: 20px"></span> nceswcd2019@sedtaifpu.org</p>
             </div>
-        </form>
+        </div>
+
+
+
+
     </div>
 </section>
 
