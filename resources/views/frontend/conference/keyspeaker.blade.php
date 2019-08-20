a9b7463abb571da52d621af3c3e94a1a8a0264b9
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-20 21:43:26 +0100

    --- Auto-Git Commit ---

---
+++ resources/views/frontend/conference/keyspeaker.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.app')
+
+@section('content')
+<section id="faq" class="section faq">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <h3 class="section-title">Speakers</h3>
+                
+                
+            </div>
+           
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-10">
+                
+            </div>
+        </div>
+</section>
+@endsection
