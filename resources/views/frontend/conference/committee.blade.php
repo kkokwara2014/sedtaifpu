26ad2105ec7ca9f0adad804e32e0153b824bc424
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-26 14:14:00 +0100

    --- Auto-Git Commit ---

--- resources/views/frontend/conference/committee.blade.php
+++ resources/views/frontend/conference/committee.blade.php
@@ -6,26 +6,31 @@
         <div class="row">
             <div class="col-md-12">
                 <h3 class="section-title">Committee Members</h3>
+               
+                Our dedicated Committee Members are working round the clock to
+                ensure that your Conference presentation will be a memorable one.
             </div>
+
         </div>
 
         <div class="row">
             <div class="col-md-12">
                 <div class="row">
-                    @forelse ($confcommittees->chunk(4) as $chunk)
+                    @forelse ($confcommittees->chunk(3) as $chunk)
                     @foreach ($chunk as $committee)
                     <div class="col-md-3">
                         <div class="speaker">
-                            <figure>
+                            {{-- <figure>
                                 <img alt="" class="img-responsive center-block"
                                     src="{{asset('bootstrap_assets/assets/images/speakers/speaker-1.jpg')}}">
-                            </figure>
-                            <h4>{{$committee->title.' '.$committee->name}}</h4>
-
-                            <p><span class="ion-briefcase"></span> {{$committee->designation}}</p>
-                            <p><span class="ion-location"></span> {{$committee->institution}}</p>
-                            <p><span class="ion-mail"></span> {{$committee->phone}}</p>
-                            <p>{{$committee->email}}</p>
+                            </figure> --}}
+                            <h4 style="text-align: justify">{{$committee->title.' '.$committee->name}}</h4>
+                            <p style="text-align: justify">
+                                <small><span class="ion-briefcase"></span> {{$committee->designation}}</small><br>
+                                <small><span class="ion-location"></span> {{$committee->institution}}</small><br>
+                                <small><span class="ion-iphone"></span> {{$committee->phone}}</small><br>
+                                <small><span class="ion-email"></span> {{$committee->email}}</small>
+                            </p>
 
                             {{-- <ul class="social-block">
                                 <li><a href=""><i class="ion-social-twitter"></i></a></li>
@@ -37,7 +42,7 @@
                     </div><!-- /.col-md-3 -->
                     @endforeach
                     @empty
-                        <p class="alert alert-info">No Committee Member has been added!</p>
+                    <p class="alert alert-info">No Committee Member has been added!</p>
                     @endforelse
 
                     {{-- <div class="col-md-3">
--- resources/views/frontend/conference/payment.blade.php
+++ resources/views/frontend/conference/payment.blade.php
@@ -4,32 +4,74 @@
 <section id="faq" class="section faq">
     <div class="container">
         <div class="row">
-            <div class="col-md-10">
+            <div class="col-md-11">
                 <h3 class="section-title">Payment Details</h3>
 
                 You can make payment in favor of School of Environmental and Design via the
                 following ways :
                 <br><br>
+                <div class="row">
+                    <div class="col-md-11">
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
-                <ol>
-                    <li>
-                        <strong>Bank Deposit</strong> <br>
-                        Teller payment in favor of “School of Environmental Design and Technology Conference," payable
-                        at Fidelity Bank, Akanu Ibiam Federal Polytechnic, Unwana.
-                    </li>
-                    <li>
-                        <strong>Electronic Transfer </strong> <br>
-                        Account Name : "School of Environmental Design and Technology Conference" <br>
-                        Account Number : 6060188024
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <h4 class="panel-title">
+                                        <a class="faq-toggle collapsed" role="button" data-toggle="collapse"
+                                            data-parent="#accordion" href="#collapseOne" aria-expanded="true"
+                                            aria-controls="collapseOne"> Mode of Payment</a>
+                                    </h4>
+                                </div>
 
-                    </li>
-                    <li>
-                        <strong>Electronic Transfer </strong> <br>
-                        Account Name : "School of Environmental Design and Technology Conference" <br>
-                        Account Number : 6060188024
+                                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel"
+                                    aria-labelledby="headingOne">
+                                    <div class="panel-body">
+                                        <h3>Mode of Payment</h3>
 
-                    </li>
-                </ol>
+                                        <ol>
+                                            <li>
+                                                <strong>Bank Deposit</strong> <br>
+                                                Teller payment in favor of “School of Environmental Design and
+                                                Technology Conference," payable
+                                                at Fidelity Bank, Akanu Ibiam Federal Polytechnic, Unwana.
+                                            </li>
+                                            <li>
+                                                <strong>Electronic Transfer </strong> <br>
+                                                Account Name : "School of Environmental Design and Technology
+                                                Conference" <br>
+                                                Account Number : 6060188024
+
+                                            </li>
+                                            <li>
+                                                <strong>Electronic Transfer </strong> <br>
+                                                Account Name : "School of Environmental Design and Technology
+                                                Conference" <br>
+                                                Account Number : 6060188024
+                                            </li>
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingTwo">
+                                    <h4 class="panel-title">
+                                        <a class="faq-toggle collapsed" role="button" data-toggle="collapse"
+                                            data-parent="#accordion" href="#collapseTwo" aria-expanded="false"
+                                            aria-controls="collapseTwo"> Payment Category</a>
+                                    </h4>
+                                </div>
+
+                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"
+                                    aria-labelledby="headingTwo">
+                                    <div class="panel-body">Payment Category</div>
+                                    
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
